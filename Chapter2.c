dab3f45475275d49e71814d36817850b8c965699
yilun.ylc <yilun.ylc@gmail.com>
2020-05-30 22:12:13 +0800

    Example of Chapter 2.C programming language standard

---
+++ Chapter2.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	printf("prog 2_2\n");
+	int i=2;
+	if(i<5)
+	{
+		printf("變數i的質小於5");
+		printf("\n");
+	}
+	
+	printf("------------------------------\n");
+	printf("prog 2_Ex_3\n");
+	printf("You are my best friend\n");
+	
+	printf("------------------------------\n");
+	printf("prog 2_Ex_4\n");
+	printf("See you tomorrow.\n");
+	printf("Have a good night.\n");
+	
+	printf("------------------------------\n");
+	printf("prog 2_Ex_5\n");
+	printf("5+12=%d\n",(5+12));
+	
+	printf("------------------------------\n");
+	printf("prog 2_Ex_6\n");
+	printf("6+7+24=%d\n",(6+7+24));
+	
+	printf("------------------------------\n");
+	printf("prog 2_Ex_10\n");
+	if(i<5)
+	{
+		printf("變數i的質小於5\n");
+	}
+	
+	printf("------------------------------\n");
+	printf("prog 2_Ex_11\n");
+	printf("*\n");
+	printf("**\n");
+	printf("***\n");
+	printf("****\n");
+	printf("*****\n");
+	
+	system("pause");
+	return 0;
+}
