18fef20c31bc99ee613dee72fbe66935a742a9e9
yilun.ylc <yilun.ylc@gmail.com>
2020-05-30 22:05:50 +0800

    Example of Chapter 1.Introduce to C language

---
+++ Chapter1.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	printf("prog 1_1\n");
+	printf("Hello C!\n");
+	printf("Hello World!\n");
+	
+	printf("------------------------------\n");
+	printf("prog 1_Ex_13\n");
+	printf("我愛C語言\n");
+	
+	printf("------------------------------\n");
+	printf("prog 1_Ex_14\n");
+	printf("我愛C語言\n");
+	printf("這是我的第一個C語言程式\n");
+	
+	printf("------------------------------\n");
+	printf("prog 1_Ex_15\n");
+	printf("*\n");
+	printf("**\n");
+	printf("***\n");
+	printf("****\n");
+	printf("*****\n");
+	
+	printf("------------------------------\n");
+	printf("prog 1_Ex_16\n");
+	printf("    *\n");
+	printf("   ***\n");
+	printf("  *****\n");
+	printf(" *******\n");
+	printf("*********\n");
+	
+	printf("------------------------------\n");
+	printf("prog 1_Ex_17\n");
+	printf("********\n");
+	printf("********\n");
+	printf("**    **\n");
+	printf("**    **\n");
+	printf("********\n");
+	printf("********\n");
+	
+	system("pause");
+	return 0;
+}
